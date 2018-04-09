6b820ff0fe0b1031a9b2c674ea1003037c18e781
Martijn de Boer <github@sexybiggetje.nl>
2018-04-09 19:37:15 +0200

    ATtempt to add multidomain support

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Birdeatsbaby lyrics generator
 
 Generates a new set of lyrics based on previously written lyrics.
+Due to popular demand it now supports Feline & Strange lyrics as well. Make sure the domain is in the configuration file.
 
 The current iteration uses Markov chains, which are probably the easiest way to do this. This is just step one.
 
-A public instance runs on [bebgen.theimpossibleastronaut.com](https://bebgen.theimpossibleastronaut.com).
+A public instance runs on [bebgen.theimpossibleastronaut.com](https://bebgen.theimpossibleastronaut.com) and for Feline & Strange it's on [fsgen.theimpossibleastronaut.com](https://fsgen.theimpossibleastronaut.com).
 
 ## Acknowledgments
 Ofcourse the lyrics are owned by their respective writers. This application uses RiTa for the Markov chains.
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var restify = require( 'restify' );
 var bindIp = '0.0.0.0', bindPort = '8081';
 var fs = require('fs');
 var rita = require('rita');
-var markov = new rita.RiMarkov(3);
+var markovs = [];
 
 var server = restify.createServer( {
 
@@ -12,6 +12,7 @@ var server = restify.createServer( {
 
 } );
 
+var configuration = JSON.parse( fs.readFileSync( 'configuration.json' ) );
 annotateModel();
 
 server.get( {path: '/*'}, generateLyrics );
@@ -27,9 +28,14 @@ server.listen( bindPort, bindIp, function() {
 
 function generateLyrics( req, res, next ) {
 
-	var str = "Birdeatsbaby lyrics generator.\r\nEvery time you refresh this page, a new lyric is generated using computational linguistics based on previous lyrics.\r\nThis is a first version, so please forgive any problems with the generated text.\r\n-------------------\r\n\r\n";
+	res.setHeader("Content-Type", "text/plain; charset=utf-8");
 
-	var strings = markov.generateSentences(
+	var config = getConfiguration( req );
+	var ind = config.index;
+
+	var str = config.text;
+
+	var strings = markovs[ind].generateSentences(
 		Math.floor( Math.random() * (24 - 14 + 1) ) + 14
 	);
 
@@ -45,30 +51,64 @@ function generateLyrics( req, res, next ) {
 
 }
 
+var currentConfig;
 function annotateModel() {
-	readFiles('lyrics/', function(filename,content){
-		console.log("Loaded " + filename);
-		markov.loadText(content);
-	},
-	function(err){
-		throw err;
-	});
+
+	for ( var i = 0; i < configuration.apps.length; i++ ) {
+		currentConfig = i;
+
+		configuration.apps[currentConfig].index = i;
+
+		console.log( "Setting up " + configuration.apps[currentConfig].domain );
+		markovs[i] = new rita.RiMarkov(3);
+
+		readFiles(configuration.apps[currentConfig], function(config,filename,content){
+			console.log("Loaded " + filename + " into " + config.domain);
+
+			markovs[config.index].loadText(content);
+		},
+		function(err){
+			throw err;
+		});
+
+	}
 }
 
-function readFiles(dirname, onFileContent, onError) {
-  fs.readdir(dirname, function(err, filenames) {
+function readFiles(config, onFileContent, onError) {
+  fs.readdir(config.folder, function(err, filenames) {
     if (err) {
       onError(err);
       return;
     }
     filenames.forEach(function(filename) {
-      fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+      fs.readFile(config.folder + "/" + filename, 'utf-8', function(err, content) {
         if (err) {
           onError(err);
           return;
         }
-        onFileContent(filename, content);
+        onFileContent(config, filename, content);
       });
     });
   });
+}
+
+function getConfiguration( req ) {
+	var domain = "";
+
+	if ( req.headers.origin !== undefined ) {
+		domain = req.headers.origin;
+	} else if ( req.headers.host !== undefined ) {
+		domain = req.headers.host;
+	}
+
+	domain = domain.split(":");
+	domain = domain[0];
+
+	for ( var i = 0; i < configuration.apps.length; i++ ) {
+		if ( configuration.apps[i].domain.toLowerCase() == domain.toLowerCase() ) {
+			return configuration.apps[i];
+		}
+	}
+
+	return configuration.apps[0];
 }
